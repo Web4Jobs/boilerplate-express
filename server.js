520fa861c5c0cbc11d077cc4a042ea2ad812843b
Web4Jobs <a.achlaih@web4jobs.ma>
2026-01-28 10:52:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const myApp = require('./myApp');
 const express = require('express');
 const app = express();
 
-if (!process.env.DISABLE_XORIGIN) {
-  app.use((req, res, next) => {
-    const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    const origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
+// if (!process.env.DISABLE_XORIGIN) {
+//   app.use((req, res, next) => {
+//     const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+//     const origin = req.headers.origin || '*';
+//     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+//          console.log(origin);
+//          res.setHeader('Access-Control-Allow-Origin', origin);
+//          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     }
+//     next();
+//   });
+// }
 
 const port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, () => {
